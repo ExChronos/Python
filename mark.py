a03fdf61f6b36b1a2685b08ce134c44fc015efad
Nathan <retrograde1779@gmail.com>
2023-05-23 11:41:40 +0300

    strange commit

--- mark.py
+++ mark.py
@@ -1,1 +1,15 @@
-print("hello world")
+def rsum(L):
+    if not L:
+        return 0
+    else:
+        return L[0] + rsum(L[1:])
+    
+print(rsum([1,2,3,4,5]))
+
+def csum(L, sum):
+
+    for i in L:
+        sum += i
+
+csum([1,2,3,4], 0)
+print(sum)
